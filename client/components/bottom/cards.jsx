dad89d5c1ca3b7742d34c7fc29ca3b5025661f46
yarnett <86074276+yarnett@users.noreply.github.com>
2021-08-02 12:48:55 -0400

    midday changes

--- client/components/bottom/cards.jsx
+++ client/components/bottom/cards.jsx
@@ -1,12 +1,17 @@
-// import React from "react";
+import React from "react";
 
-// const testState = {
-//     name : 'Yolan',
-//     hobby: 'Kickboxing'
-// }
+const testState = {
+    name : 'Yolan',
+    hobby: 'Kickboxing'
+}
 
-// const Cards = Obj => (
+const Cards = Obj => {
+    return (
+        <div className = "itemCard">
+        <span> { testState.name } </span>,
+      </div>
+    );
+}
 
-// );
 
-// export default Cards;
+export default Cards;
