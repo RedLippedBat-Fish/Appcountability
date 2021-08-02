12fe426d20714fce7eb14ef003ff316d01b4fc25
Jae Kim <jaekimr@gmail.com>
2021-08-02 07:47:28 -0400

    restore frontend

---
+++ client/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+      {/* <img src="./Assets/btnNormal.png" /> */}
+      {/* <input type="image" src="./Assets/btnNormal.png" /> */}
+    </div>
+  );
+};
+// button to sign into g
+
+export default Login;
